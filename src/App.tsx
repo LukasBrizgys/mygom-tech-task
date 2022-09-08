8f57af3d4ea6ed87048b5dcff0d101c2797104a3
Lukas Brizgys <lukenis456@gmail.com>
2022-09-08 11:57:56 +0300

    add mock data and Item type

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from "react";
-import { Dropdown } from "./components/Dropdown";
-
+import React from 'react';
+import { Dropdown } from './components/Dropdown';
+export type Item = {
+		name: string,
+		group?: string
+	}
 const App = () => {
-  return (
-    <div>
-      <Dropdown />
-    </div>
-  );
-}
+	const items : Item[] = [
+		{name: 'itemOne', group:'group One'},
+		{name: 'itemTwo', group:'group One'},
+		{name: 'itemThree', group:'group Two'},
+		{name: 'itemFour', group:'group Two'},
+		{name: 'itemFive'},
+		{name: 'itemSix'},
+		{name: 'itemSeven', group:'group Three'},
+		{name: 'itemEight'}
+	];
+	return (
+		<div>
+			<Dropdown items={items} selectionLimit={3}/>
+		</div>
+	);
+};
 
 export default App;
