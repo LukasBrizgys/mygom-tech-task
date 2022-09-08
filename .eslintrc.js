1b4d1bceb121bec1cb82a3a1385873c26cca1ee1
Lukas Brizgys <lukenis456@gmail.com>
2022-09-08 11:58:31 +0300

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,26 @@
+/* eslint-disable */
 module.exports = {
-	'env': {
-		'browser': true,
-		'es2021': true
-	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:@typescript-eslint/recommended'
-	],
-	'overrides': [
-	],
-	'parser': '@typescript-eslint/parser',
-	'parserOptions': {
-		'ecmaVersion': 'latest',
-		'sourceType': 'module'
-	},
-	'plugins': [
-		'react',
-		'@typescript-eslint'
-	],
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'windows'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		]
-	}
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'settings': {
+    "react": {
+      "version": "detect"
+    }
+  },
+  'extends': ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:storybook/recommended'],
+  'overrides': [],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': ['react', '@typescript-eslint'],
+  'rules': {
+    'indent': ['error', 'tab'],
+    'linebreak-style': ['error', 'windows'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always']
+  }
 };
